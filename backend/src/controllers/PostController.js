ef40bf079ad25cb94005747619382462d9c93217
castelogui <guilhermecastelo.mail@gmail.com>
2020-06-19 12:27:02 -0400

    add join in table users and posts

--- backend/src/controllers/PostController.js
+++ backend/src/controllers/PostController.js
@@ -2,8 +2,9 @@ const knex = require("../database");
 
 module.exports = {
   async index(request, response){
-    const posts = await knex('posts')
-      .select('*');    
+    const posts = await knex('users')
+      .join('posts', 'users.id', '=', 'posts.user_id')
+      .select( 'posts.*', 'users.username', 'users.name');  
     return response.json(posts);
   }
 }
