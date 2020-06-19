046bc212419a90749cf50ca8a7a316694834050c
castelogui <guilhermecastelo.mail@gmail.com>
2020-06-19 11:58:00 -0400

    add route for feed posts

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,11 +1,14 @@
 const express = require('express');
 
 const UserController = require('./controllers/UserController');
+const PostController = require('./controllers/PostController');
 
 const routes = express.Router();
 
 routes.get('/users', UserController.all);
 routes.get('/profile/:username', UserController.index);
 routes.post('/register', UserController.create);
+
+routes.get('/feed', PostController.index);
 
 module.exports = routes;
