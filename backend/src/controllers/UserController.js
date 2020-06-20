4309dd5dad2c0301383537a39f5eda60df4f1dc8
castelogui <guilhermecastelo.mail@gmail.com>
2020-06-20 14:42:26 -0400

    add logon from user

--- backend/src/controllers/UserController.js
+++ backend/src/controllers/UserController.js
@@ -53,5 +53,25 @@ module.exports = {
       const err = e.detail
       return response.json({ message: {err, e} });
     }
+  },
+
+  async logon(request, response){
+    // Requesita os dados para login
+    const { username, password } = request.body;
+
+    // Aguarda a conexão buscando o dados enviados do body
+    const user = await knex('users')
+      .where('username', username)
+      .andWhere('password', password)
+      .select('id','name','username','email') // seleciona dados do user em questão
+      .first();
+    
+    // Se não existir um user com aqueles dados
+    if(!user){
+      return response.status(400).json({ error: 'Username or password invalided!'})
+    }
+
+    const message = `Sucessefull login from ${username}`;
+    return response.json({ message: message, user});
   }
 }
