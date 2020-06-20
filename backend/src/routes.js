e4a106cea0db5052d974bd486a4eceaee25ff66c
castelogui <guilhermecastelo.mail@gmail.com>
2020-06-20 16:21:45 -0400

    alter route logon from session controller

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -2,13 +2,14 @@ const express = require('express');
 
 const UserController = require('./controllers/UserController');
 const PostController = require('./controllers/PostController');
+const SessionController = require('./controllers/SessionController');
 
 const routes = express.Router();
 
 routes.get('/users', UserController.all);
 routes.get('/profile/:username', UserController.index);
 routes.post('/register', UserController.create);
-routes.post('/logon', UserController.logon);
+routes.post('/logon', SessionController.index);
 
 routes.get('/feed', PostController.index);
 
