439cd6cb32449a821dc6069ebe28ed500cc82eaa
castelogui <guilhermecastelo.mail@gmail.com>
2020-06-20 14:42:38 -0400

    add route from logon

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -8,6 +8,7 @@ const routes = express.Router();
 routes.get('/users', UserController.all);
 routes.get('/profile/:username', UserController.index);
 routes.post('/register', UserController.create);
+routes.post('/logon', UserController.logon);
 
 routes.get('/feed', PostController.index);
 
