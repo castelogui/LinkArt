8c036800cf3f280248a21b618c6db91e0792a627
castelogui <guilhermecastelo.mail@gmail.com>
2020-06-20 16:31:10 -0400

    add return id for list one user

--- backend/src/controllers/UserController.js
+++ backend/src/controllers/UserController.js
@@ -7,6 +7,7 @@ module.exports = {
     const user = await knex('users')
       .where('username', username)
       .select(
+        'id',
         'name', 
         'username', 
         'occupation',
