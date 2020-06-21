0cd3e23d0f45dcda135cb53fedf38b21c6adba2e
castelogui <guilhermecastelo.mail@gmail.com>
2020-06-21 14:47:48 -0400

    add controller for detail of one post

--- backend/src/controllers/PostController.js
+++ backend/src/controllers/PostController.js
@@ -6,5 +6,20 @@ module.exports = {
       .join('posts', 'users.id', '=', 'posts.user_id')
       .select( 'posts.*', 'users.username', 'users.name');  
     return response.json(posts);
+  },
+
+  async detail(request, response){
+    const { username, id } = request.params;
+    
+    const [post] = await knex('users')
+      .join('posts', `users.id`, '=', `posts.user_id`)
+      .where('posts.id', id)
+      .andWhere('users.username', username)
+      .select('posts.*', 'users.username', 'users.name');
+    
+    if(!post){
+      return response.json({ error: `User ${username} no have this post ${id}` })
+    }
+    return response.json(post);
   }
 }
