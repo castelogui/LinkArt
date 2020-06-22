8a0a26bd7195d301792ecb3ea5ccd3fc26a6a863
castelogui <guilhermecastelo.mail@gmail.com>
2020-06-22 09:15:58 -0400

    add post update route

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -15,5 +15,6 @@ routes.post('/logon', SessionController.index);
 routes.get('/feed', PostController.index);
 routes.get('/feed/:username/:id', PostController.detail);
 routes.post('/profile/:username/post', PostController.create);
+routes.put('/profile/:username/:id', PostController.update);
 
 module.exports = routes;
