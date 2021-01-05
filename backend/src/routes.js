f435764be98acd5f45b96b922febe3732fa0477c
castelogui <guilhermecastelo.mail@gmail.com>
2021-01-05 00:25:20 -0400

    correção das rotas do PostController

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -6,16 +6,20 @@ const SessionController = require('./controllers/SessionController');
 
 const routes = express.Router();
 
-routes.get('/users', UserController.all);
-routes.get('/profile/:username', UserController.index);
-routes.put('/profile/:username', UserController.update);
+/*Routes for UserController*/ 
+routes.get('/', UserController.all);
+routes.get('/user/:username', UserController.index);
+routes.put('/user/:username', UserController.update);
 routes.post('/register', UserController.create);
-routes.post('/logon', SessionController.index);
 
-routes.get('/feed', PostController.index);
-routes.get('/feed/:username/:id', PostController.detail);
-routes.post('/profile/:username/post', PostController.create);
-routes.put('/profile/:username/:id', PostController.update);
-routes.delete('/profile/:username/:id', PostController.delete);
+/*Routes for PostController*/
+routes.get('/post/:id', PostController.onePost);
+routes.get('/post', PostController.index);
+routes.get('/post/:username/:id', PostController.detail);
+routes.post('/post/:username', PostController.create);
+routes.put('/post/:username/:id', PostController.update);
+routes.delete('/post/:username/:id', PostController.delete);
+
+routes.post('/logon', SessionController.index);
 
 module.exports = routes;
